a7edde3a1d3fd28bfccfd0e5b643b7086a66cbb8
Nicolas Asanov <98969136+naasanov@users.noreply.github.com>
2025-09-22 18:55:05 -0400

    refactoring backend (#5)

--- backend/src/modules/auth/auth_model.py
+++ backend/src/modules/auth/auth_model.py
@@ -1,20 +1,28 @@
 from typing import Optional
 
+from modules.user.user_model import UserDto
 from pydantic import BaseModel, EmailStr
 
 
-class CreateTokenDto(BaseModel):
+class LoginData(BaseModel):
     email: EmailStr
     password: str
 
 
-class RefreshTokenDto(BaseModel):
+class WebAuthDto(BaseModel):
+    user: UserDto
+    access_token: str
+
+
+class MoblieAuthDto(BaseModel):
+    user: UserDto
+    access_token: str
+    refresh_token: str
+
+
+class RefreshData(BaseModel):
     refresh_token: Optional[str] = None
 
 
 class AccessTokenDto(BaseModel):
     access_token: str
-
-
-class TokensDto(RefreshTokenDto, AccessTokenDto):
-    pass
