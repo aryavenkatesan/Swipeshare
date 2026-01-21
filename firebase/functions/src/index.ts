1a0334229a5778f2e60a3676c754209d5812468b
Nicolas Asanov <nicolas.a.asanov@gmail.com>
2026-01-21 15:05:55 -0500

    feat: archive instead of delete Listings

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -2,9 +2,9 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
-export * from "./functions/notifications";
-export * from "./functions/ratings";
-export * from "./functions/system-messages";
 export * from "./functions/forgot-password";
+export * from "./functions/notifications";
 export * from "./functions/order-transaction";
+export * from "./functions/ratings";
 export * from "./functions/resource-expiration";
+export * from "./functions/system-messages";
