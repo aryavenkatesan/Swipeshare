b25a8bf939de30a7abc5f8400ea811132f5a6dde
VidurShah <vidurshah12@gmail.com>
2026-01-28 22:10:52 -0500

    fixed things that broke from resolving merge conflicts

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -3,7 +3,6 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 export * from "./functions/notifications";
-export * from "./functions/remote-call";
 export * from "./functions/ratings";
 export * from "./functions/system-messages";
 export * from "./functions/forgot-password";
