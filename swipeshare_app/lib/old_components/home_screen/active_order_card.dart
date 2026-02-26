bf71c8f283737118650f600fd2c361cf87858dd7
Nicolas Asanov <nicolas.a.asanov@gmail.com>
2026-02-26 15:02:48 -0500

    refactor: added time formatter utils for date range and TimeOfDay => readable string

--- swipeshare_app/lib/old_components/home_screen/active_order_card.dart
+++ swipeshare_app/lib/old_components/home_screen/active_order_card.dart
@@ -84,7 +84,7 @@ class ActiveOrderCard extends StatelessWidget {
                       isCancelled
                           ? 'Cancelled'
                           : orderData.displayTime != null
-                          ? "${TimeFormatter.formatTimeOfDay(orderData.displayTime!)}    ${TimeFormatter.formatToMMDD(orderData.transactionDate)} "
+                          ? "${TimeFormatter.formatTimeOfDayString(orderData.displayTime!)}    ${TimeFormatter.formatToMMDD(orderData.transactionDate)} "
                           : "${TimeFormatter.formatToMMDD(orderData.transactionDate)}, TBD",
                       style: GreyHeaderStyle,
                     ),
